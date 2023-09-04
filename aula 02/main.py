a67817e64f84d401c6e245f4705abe4a93c02346
Lucas Wesley Mendes de Meneses Fragoso <lucasfragoso@academicos.com>
2023-09-04 19:46:31 -0300

    feat: add exercicio um aula 02

---
+++ aula 02/main.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    # 1
+    number = float(input('digite um número: '))
+
+    if number > 10:
+        print('seu número é maior que 10')
+    elif number == 10:
+        print('seu número é igual a 10')
+    else:
+        print('seu número é menor que 10')
+
+    
