51b0ddfc217c3926f2948118dfbcaaa381d633da
Lucas Wesley Mendes de Meneses Fragoso <lucasfragoso@academicos.com>
2023-09-04 20:19:01 -0300

    feat: add exercicio 3 aula 02

--- aula 02/main.py
+++ aula 02/main.py
@@ -10,16 +10,25 @@ if __name__ == "__main__":
     #     print('seu número é menor que 10')
 
     # 2
-    nota1 = float(input("Digite a primeira nota: "))
-    nota2 = float(input("Digite a segunda nota: "))
+    # nota1 = float(input("Digite a primeira nota: "))
+    # nota2 = float(input("Digite a segunda nota: "))
 
-    media = (nota1 + nota2)/ 2
+    # media = (nota1 + nota2)/ 2
 
-    if 10 >= media >= 6:
-        print(f'Você foi aprovado,  com média: {media}')
-    elif 0 <= media < 6:
-        print(f'Você foi reprovado,  com média: {media}')
-    else:
-        print(f'Valor da media inválido, média: {media}')
+    # if 10 >= media >= 6:
+    #     print(f'Você foi aprovado,  com média: {media}')
+    # elif 0 <= media < 6:
+    #     print(f'Você foi reprovado,  com média: {media}')
+    # else:
+    #     print(f'Valor da media inválido, média: {media}')
 
+    # 3
+    value1 = float(input('digite um valor: '))
+    value2 = float(input('digite outro valor: '))
     
+    if value1 > value2:
+        print(f'O valor1:{value1} é maior que o valor2:{value2}')
+    elif value2 > value1:
+        print(f'O valor2:{value2} é maior que o valor1:{value1}')
+    else:
+        print(f'O valor1:{value1} é igual ao valor2:{value2}')
