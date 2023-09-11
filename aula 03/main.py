7f8b41ead08c70b19ddb511b1f8f96458fcf8dc2
Lucas Wesley Mendes de Meneses Fragoso <lucasfragoso@academicos.com>
2023-09-11 20:43:14 -0300

    feat: add aula 03

---
+++ aula 03/main.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+        
+    # ciclos = 5
+
+    # while ciclos > 0:
+    #     print('Por Favor, altere sua senha.'
+    #         f'\n Faltam apenas {ciclos} dias'
+    #         f'\n para ela ser bloqueada'
+    #     )
+    #     print('\n')
+    #     ciclos -= 1
+
+    for i in range(5, 0, -1):
+        print('Por Favor, altere sua senha.'
+            f'\n Faltam apenas {i} dias'
+            f'\n para ela ser bloqueada'
+        )
+        print('\n')
